a712ad5f4a67201bcc474847331b419e00ea11b5
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2021-06-03 13:11:18 -0500

    Fixing license headers

--- src/main/java/net/webtide/tools/github/iters/ListUsersSpliterator.java
+++ src/main/java/net/webtide/tools/github/iters/ListUsersSpliterator.java
@@ -1,3 +1,15 @@
+//
+// ========================================================================
+// Copyright (c) Webtide LLC and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License, Version 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: Apache-2.0
+// ========================================================================
+//
+
 package net.webtide.tools.github.iters;
 
 import java.io.IOException;
