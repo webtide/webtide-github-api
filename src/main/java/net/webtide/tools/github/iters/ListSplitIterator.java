d2662ff8586121f1c60232fed5d118453afb5c1b
olivier lamy <olamy@apache.org>
2021-06-10 06:49:25 +1000

    add missing license header
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/main/java/net/webtide/tools/github/iters/ListSplitIterator.java
+++ src/main/java/net/webtide/tools/github/iters/ListSplitIterator.java
@@ -1,8 +1,17 @@
+//
+// ========================================================================
+// Copyright (c) Webtide LLC and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License, Version 2.0 which is available at
+// https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: Apache-2.0
+// ========================================================================
+//
+
 package net.webtide.tools.github.iters;
 
-import net.webtide.tools.github.Card;
-import net.webtide.tools.github.Cards;
-import net.webtide.tools.github.Column;
 import net.webtide.tools.github.GitHubApi;
 
 import java.io.IOException;
